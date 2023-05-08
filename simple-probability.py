9807a2bf70e8d228e01af006f13df4d46c3835e7
ernestonr1 <ernstras@protonmail.com>
2023-05-08 11:37:35 +0200

    Initial commit

---
+++ simple-probability.py
@@ -0,0 +1,26 @@
+# Simple probability
+
+# Throwing a perfect die
+"""
+The probability of getting 1 in one throw is 1/6.
+"""
+print(1/6)
+
+"""
+The probability of getting any number in one throw is
+1/6. The likelihood of getting a 
+"""
+
+"""
+What is the probability of not throwing a 1.
+1 - 1/6
+The sum of all probabilities must add up to 1. The
+probablity if not getting a 1 is the probablity
+"""
+
+"""
+We roll the die twice. What is the probability of getting
+1 in both throws?
+(1/6) * (1/6)
+"""
+print( (1/6) * (1/6) )
